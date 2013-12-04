61a73fb3a2664a6e284c9e40918e58db69d4f024
David Barragán Merino <david.barragan@kaleidos.net>
2013-12-04 13:40:42 +0100

    Ordered all methods of data module

--- gmncurses/data.py
+++ gmncurses/data.py
@@ -8,11 +8,22 @@ gmncurses.data
 from datetime import datetime
 
 
+# project_stats - Points
+
 def total_points(project_stats):
     return project_stats.get("total_points", 0)
 
-def total_sprints(project_stats):
-    return project_stats.get("total_milestones", 0)
+def assigned_points(project_stats):
+    return project_stats.get("assigned_points", 0)
+
+def defined_points(project_stats):
+    return project_stats.get("defined_points", 0)
+
+def defined_points_percentage(project_stats):
+    try:
+        return defined_points(project_stats) * 100 / total_points(project_stats)
+    except ZeroDivisionError:
+        return 0
 
 def closed_points(project_stats):
     return  project_stats.get("closed_points", 0)
@@ -23,22 +34,19 @@ def closed_points_percentage(project_stats):
     except ZeroDivisionError:
         return 0
 
+
+# project_stats - Sprints
+
+def total_sprints(project_stats):
+    return project_stats.get("total_milestones", 0)
+
 def completed_sprints(project):
     milestones = project.get("list_of_milestones", [])
     now = datetime.now()
     return [m for m in milestones if date(m["finish_date"]) < now]
 
-def date(text, date_format="%Y-%m-%d"):
-    return datetime.strptime(text, date_format)
 
-def defined_points(project_stats):
-    return project_stats.get("defined_points", 0)
-
-def defined_points_percentage(project_stats):
-    try:
-        return defined_points(project_stats) * 100 / total_points(project_stats)
-    except ZeroDivisionError:
-        return 0
+# project - Sprints
 
 def current_sprint(project):
     milestones = project.get("list_of_milestones", [])
@@ -52,15 +60,26 @@ def current_sprint_name(project):
         return milestones[-1].get("name", "unknown")
     return "-----"
 
+# project - Roles
+
 def computable_roles(project):
     # FIXME
     return [r for r in project["roles"] if r["computable"]] if "roles" in project else []
+
+
+# User Stories data
 
 def us_ref(us):
     return us.get("ref", "--")
 
 def us_subject(us):
     return us.get("subject", "------")
+
+def us_total_points(us):
+    return us.get("total_points", "--")
+
+
+# us, project, computable roles - US points
 
 def us_points_by_role(us, project, roles):
     us_points = us.get("points", [])
@@ -75,5 +94,9 @@ def us_points_by_role(us, project, roles):
             points.append(project_points[str(default_point)]["name"])
     return points
 
-def us_total_points(us):
-    return us.get("total_points", "--")
+
+# Misc
+
+def date(text, date_format="%Y-%m-%d"):
+    return datetime.strptime(text, date_format)
+
