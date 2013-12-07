c5753e3cc5a8832235c0792861b6d601e5abde75
David Barragán Merino <david.barragan@kaleidos.net>
2013-12-07 22:00:25 +0100

    Added method to call the api and get the issues stats

--- gmncurses/executor.py
+++ gmncurses/executor.py
@@ -13,17 +13,37 @@ class Executor(object):
         self.client = client
         self.pool = ThreadPoolExecutor(2)
 
+    # Auth
     def login(self, username, password):
         return self.pool.submit(self.client.login, username, password)
 
+    # Project
     def projects(self):
         return self.pool.submit(self.client.get_projects)
 
     def project_detail(self, project):
         return self.pool.submit(self.client.get_project, id=project["id"])
 
+    def project_stats(self, project):
+        return self.pool.submit(self.client.get_project_stats, id=project["id"])
+
+    def project_issues_stats(self, project):
+        return self.pool.submit(self.client.get_project_issues_stats, id=project["id"])
+
+    # Milestones
+    #TODO
+
+    # User Stories
     def unassigned_user_stories(self, project):
         return self.pool.submit(self.client.get_user_stories, params={"project": project["id"],
                                                                       "milestone__isnull": True})
-    def project_stats(self, project):
-        return self.pool.submit(self.client.get_project_stats, id=project["id"])
+
+    # Task
+    #TODO
+
+    # Issues
+    def issues(self, project):
+        return self.pool.submit(self.client.get_issues, params={"project": project["id"]})
+
+    # Wiki
+    #TODO
