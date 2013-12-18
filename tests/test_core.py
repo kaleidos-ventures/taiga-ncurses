2af140143375c53c26a89c043234f899c54606ce
David Barragán Merino <david.barragan@kaleidos.net>
2013-12-18 20:39:34 +0100

    Fixed test

--- tests/test_core.py
+++ tests/test_core.py
@@ -24,7 +24,7 @@ def test_if_client_is_authenticated_the_projects_view_is_shown_on_startup():
 
 def test_transitioning_from_projects_to_project_detail_and_project_backlog():
     projects = factories.projects()
-    project = projects[0]
+    project = factories.project()
     project_f = Future()
     us = Future()
     stats = Future()
