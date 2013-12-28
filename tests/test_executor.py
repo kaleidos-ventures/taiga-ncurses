62dcc5a6df1344242a0301274aa83db7f43dfe0e
David Barragán Merino <david.barragan@kaleidos.net>
2013-12-28 11:16:39 +0100

    Added some tests

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -4,12 +4,14 @@ from concurrent.futures import Future
 from gmncurses import executor
 
 
+# Auth
 def test_login_method_returns_a_future():
     client = mock.Mock()
     e = executor.Executor(client)
     f = e.login("admin", "123123")
     assert isinstance(f, Future)
 
+# Project
 def test_projects_method_returns_a_future():
     client = mock.Mock()
     e = executor.Executor(client)
@@ -28,8 +30,73 @@ def test_project_stats_method_returns_a_future():
     f = e.project_stats({"id": 123123})
     assert isinstance(f, Future)
 
-def test_user_stories_method_returns_a_future():
+def test_project_issues_stats_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.project_issues_stats({"id": 123123})
+    assert isinstance(f, Future)
+
+# Milestones
+def test_milestone_stats_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.milestone_stats(123123, {"id": 123123})
+    assert isinstance(f, Future)
+
+# User Stories
+def test_create_user_story_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.create_user_story({"subject": "Foo Bar"})
+    assert isinstance(f, Future)
+
+def test_update_user_story_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.update_user_story({"id": 123123}, {"subject": "Bar Foo"})
+    assert isinstance(f, Future)
+
+def test_delete_user_story_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.delete_user_story({"id": 123123})
+    assert isinstance(f, Future)
+
+def test_update_user_stories_order_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.update_user_stories_order([{"id": 123123}, {"id": 456456}], {"id": 123123})
+    assert isinstance(f, Future)
+
+def test_unassigned_user_stories_method_returns_a_future():
     client = mock.Mock()
     e = executor.Executor(client)
     f = e.unassigned_user_stories({"id": 123123})
     assert isinstance(f, Future)
+
+def test_user_stories_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.user_stories(123123, {"id": 123123})
+
+# Task
+def test_milestone_tasks_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.milestone_tasks(123123, {"id": 123123})
+    assert isinstance(f, Future)
+
+# Issues
+def test_issues_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.issues({"id": 123123}, [])
+    assert isinstance(f, Future)
+
+# Wiki
+def test_wiki_pages_method_returns_a_future():
+    client = mock.Mock()
+    e = executor.Executor(client)
+    f = e.wiki_pages({"id": 123123})
+    assert isinstance(f, Future)
+
