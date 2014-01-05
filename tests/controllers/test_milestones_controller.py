85ede8cd29b028c3814a222688987197246015e3
David Barragán Merino <david.barragan@kaleidos.net>
2014-01-05 11:12:45 +0100

    Added tests

--- tests/controllers/test_milestones_controller.py
+++ tests/controllers/test_milestones_controller.py
@@ -56,3 +56,59 @@ def test_sprints_controller_reload():
     assert executor.milestone_stats.call_count == 1
     assert executor.user_stories.call_count == 1
     assert executor.milestone_tasks.call_count == 1
+
+def test_sprint_controller_show_the_milestone_selector_popup():
+    project = factories.project()
+    project_view = views.projects.ProjectDetailView(project)
+    executor = factories.patched_executor()
+    _ = mock.Mock()
+    project_detail_controller = controllers.projects.ProjectDetailController(project_view, executor, _)
+    project_detail_controller.handle(config.ProjectKeys.MILESTONES)
+
+    assert not hasattr(project_detail_controller.view.sprint, "milestone_selector_popup")
+    project_detail_controller.handle(config.ProjectBacklogKeys.MOVE_US_TO_MILESTONE)
+    assert hasattr(project_detail_controller.view.sprint, "milestone_selector_popup")
+
+def test_sprint_controller_close_the_milestone_selector_popup():
+    project = factories.project()
+    project_view = views.projects.ProjectDetailView(project)
+    executor = factories.patched_executor()
+    _ = mock.Mock()
+    project_detail_controller = controllers.projects.ProjectDetailController(project_view, executor, _)
+    project_detail_controller.handle(config.ProjectKeys.MILESTONES)
+    project_detail_controller.handle(config.ProjectMilestoneKeys.CHANGE_TO_MILESTONE)
+
+    assert hasattr(project_detail_controller.view.sprint, "milestone_selector_popup")
+    milestone_selector_popup = project_detail_controller.view.sprint.milestone_selector_popup
+    signals.emit(milestone_selector_popup.cancel_button, "click")
+    assert not hasattr(project_detail_controller.view.sprint, "milestone_selector_popup")
+
+def test_sprint_controller_change_to_another_milestone():
+    project = factories.project()
+    project_view = views.projects.ProjectDetailView(project)
+    project_view.sprint.notifier = mock.Mock()
+    executor = factories.patched_executor()
+    _ = mock.Mock()
+    project_detail_controller = controllers.projects.ProjectDetailController(project_view, executor, _)
+    project_detail_controller.handle(config.ProjectKeys.MILESTONES)
+    project_detail_controller.handle(config.ProjectMilestoneKeys.CHANGE_TO_MILESTONE)
+    milestone_selector_popup = project_detail_controller.view.sprint.milestone_selector_popup
+    project_view.sprint.notifier.reset_mock()
+    executor.milestone.reset_mock()
+    executor.milestone_stats.reset_mock()
+    executor.user_stories.reset_mock()
+    executor.milestone_tasks.reset_mock()
+
+    assert project_view.sprint.notifier.info_msg.call_count == 0
+    assert executor.milestone.call_count == 0
+    assert executor.milestone_stats.call_count == 0
+    assert executor.user_stories.call_count == 0
+    assert executor.milestone_tasks.call_count == 0
+    signals.emit(milestone_selector_popup.options[2], "click")
+    assert project_view.sprint.notifier.info_msg.call_count == 1
+    assert executor.milestone.call_count == 1
+    assert executor.milestone_stats.call_count == 1
+    assert executor.user_stories.call_count == 1
+    assert executor.milestone_tasks.call_count == 1
+    assert (project_detail_controller.sprint.milestone["id"] ==
+            milestone_selector_popup.project["list_of_milestones"][-1]["id"])
