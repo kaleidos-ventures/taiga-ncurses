23a4d290d28f4142592b43d122be8093002eab33
David Barragán Merino <david.barragan@kaleidos.net>
2014-02-16 11:18:56 +0100

    Fix typos

--- tests/controllers/test_issues_controller.py
+++ tests/controllers/test_issues_controller.py
@@ -152,12 +152,12 @@ def test_issues_controller_submit_new_issue_form_with_errors():
     assert project_view.issues.notifier.error_msg.call_count == 1
 
 def test_issues_controller_submit_new_issue_form_successfully():
-    us_subject = "Create a new user story"
+    issue_subject = "Create a new issue"
     project = factories.project()
     project_view = views.projects.ProjectDetailView(project)
     project_view.issues.notifier = mock.Mock()
     executor = factories.patched_executor(create_issue_response=factories.future(
-                           factories.successful_create_issue_response(us_subject)))
+                           factories.successful_create_issue_response(issue_subject)))
     _ = mock.Mock()
     project_detail_controller = controllers.projects.ProjectDetailController(project_view, executor, _)
     project_detail_controller.handle(config.ProjectKeys.ISSUES)
@@ -165,12 +165,12 @@ def test_issues_controller_submit_new_issue_form_successfully():
     form = project_detail_controller.view.issues.issue_form
     project_view.issues.notifier.reset_mock()
 
-    form._subject_edit.set_edit_text(us_subject)
+    form._subject_edit.set_edit_text(issue_subject)
     signals.emit(form.save_button, "click")
     assert project_view.issues.notifier.info_msg.call_count == 1
-    assert executor.create_issue.call_args.call_list()[0][0][0]["subject"] == us_subject
+    assert executor.create_issue.call_args.call_list()[0][0][0]["subject"] == issue_subject
     assert executor.create_issue.call_count == 1
-    assert executor.create_issue.return_value.result()["subject"] == us_subject
+    assert executor.create_issue.return_value.result()["subject"] == issue_subject
 
 def test_issues_controller_show_the_edit_issue_form():
     project = factories.project()
@@ -216,12 +216,12 @@ def test_issues_controller_submit_the_edit_issue_form_with_errors():
     assert project_view.issues.notifier.error_msg.call_count == 1
 
 def test_issues_controller_submit_edit_issue_form_successfully():
-    us_subject = "Update a user story"
+    issue_subject = "Update a issue"
     project = factories.project()
     project_view = views.projects.ProjectDetailView(project)
     project_view.issues.notifier = mock.Mock()
     executor = factories.patched_executor(update_issue_response=factories.future(
-                           factories.successful_update_issue_response(us_subject)))
+                           factories.successful_update_issue_response(issue_subject)))
     _ = mock.Mock()
     project_detail_controller = controllers.projects.ProjectDetailController(project_view, executor, _)
     project_detail_controller.handle(config.ProjectKeys.ISSUES)
@@ -229,14 +229,14 @@ def test_issues_controller_submit_edit_issue_form_successfully():
     form = project_detail_controller.view.issues.issue_form
     project_view.issues.notifier.reset_mock()
 
-    form._subject_edit.set_edit_text(us_subject)
+    form._subject_edit.set_edit_text(issue_subject)
 
     signals.emit(form.save_button, "click")
     assert project_view.issues.notifier.info_msg.call_count == 1
     assert (executor.update_issue.call_args.call_list()[0][0][0]["id"] == form.issue["id"])
-    assert executor.update_issue.call_args.call_list()[0][0][1]["subject"] == us_subject
+    assert executor.update_issue.call_args.call_list()[0][0][1]["subject"] == issue_subject
     assert executor.update_issue.call_count == 1
-    assert executor.update_issue.return_value.result()["subject"] == us_subject
+    assert executor.update_issue.return_value.result()["subject"] == issue_subject
 
 def test_issues_controller_delete_issue_with_errors():
     project = factories.project()
