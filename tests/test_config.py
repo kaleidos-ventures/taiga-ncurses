28714db63cef53414384d23fedc99a71a0a7bce6
David Barragán Merino <david.barragan@kaleidos.net>
2014-04-06 21:08:22 +0200

    Add some tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ from taiga_ncurses.config import DEFAULTS, Configuration
 SAMPLE_HOST = {
     "scheme": "http",
     "domain": "localhost",
-    "port": 9000,
+    "port": 9000
 }
 
 SAMPLE_CONFIG = """
@@ -16,7 +16,11 @@ SAMPLE_CONFIG = """
 scheme = {scheme}
 domain = {domain}
 port = {port}
+
+[site]
+domain = {domain}
 """.format(**SAMPLE_HOST)
+
 
 SAMPLE_AUTH = {
     "token": "42",
@@ -48,6 +52,17 @@ def test_configuration_load_host_from_file():
                                                               domain=SAMPLE_HOST["domain"],
                                                               port=SAMPLE_HOST["port"])
 
+def test_configuration_load_site_from_file():
+    _ = tempfile.NamedTemporaryFile()
+    config_file = tempfile.NamedTemporaryFile(mode='w+', encoding='utf-8', delete=False)
+    config_file.file.write(SAMPLE_CONFIG)
+    config_file.close()
+    config = Configuration(config_file=config_file.name, auth_config_file=_)
+    config.load()
+    os.remove(config_file.name)
+    _.close()
+    assert config.site == str(SAMPLE_HOST["domain"])
+
 def test_configuration_auth_property_is_none_when_no_token_is_loaded():
     _ = tempfile.NamedTemporaryFile()
     config = Configuration(config_file=_,
@@ -78,9 +93,11 @@ def test_configuration_save_to_file():
     parser.read(config_file.name, encoding="utf-8")
     os.remove(config_file.name)
     assert "host" in parser._sections
+    assert "site" in parser._sections
     assert "auth" not in parser._sections
     for k, v in SAMPLE_HOST.items():
         assert str(v) == parser._sections["host"][k]
+    assert str(SAMPLE_HOST["domain"]) == parser._sections["site"]["domain"]
 
 def test_auth_configuration_save_to_file():
     auth_config_file = tempfile.NamedTemporaryFile(mode='w+', encoding='utf-8', delete=False)
@@ -94,6 +111,7 @@ def test_auth_configuration_save_to_file():
     os.remove(auth_config_file.name)
     assert "auth" in parser._sections
     assert "host" not in parser._sections
+    assert "site" not in parser._sections
     assert "keys" not in parser._sections
     assert "colors" not in parser._sections
     assert SAMPLE_AUTH["token"] == parser._sections["auth"]["token"]
