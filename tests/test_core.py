af37022e96e38d609af1e7d3b8c17c9b522d37bf
David Barragán Merino <david.barragan@kaleidos.net>
2014-10-25 16:06:54 +0200

    Fix core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 from concurrent.futures import Future
 from unittest import mock
 
-from taiga_ncurses.config import Configuration
+from taiga_ncurses.config import settings
 from taiga_ncurses.core import TaigaCore, StateMachine
 from taiga_ncurses import controllers
 
@@ -10,15 +10,13 @@ from . import factories
 
 def test_if_client_is_not_authenticated_the_login_view_is_shown_on_startup():
     executor = factories.patched_executor()
-    configuration = Configuration()
-    core = TaigaCore(executor, configuration, authenticated=False)
+    core = TaigaCore(executor, settings, authenticated=False)
     assert isinstance(core.controller, controllers.auth.LoginController)
     assert core.state_machine.state == StateMachine.LOGIN
 
 def test_if_client_is_authenticated_the_projects_view_is_shown_on_startup():
     executor = factories.patched_executor()
-    configuration = Configuration()
-    core = TaigaCore(executor, configuration, authenticated=True, draw=False)
+    core = TaigaCore(executor, settings, authenticated=True, draw=False)
     assert isinstance(core.controller, controllers.projects.ProjectsController)
     assert core.state_machine.state == StateMachine.PROJECTS
 
@@ -31,8 +29,7 @@ def test_transitioning_from_projects_to_project_detail_and_project_backlog():
     executor = factories.patched_executor(project_detail=project_f,
                                           unassigned_user_stories=us,
                                           project_stats=stats,)
-    configuration = Configuration()
-    core = TaigaCore(executor, configuration, authenticated=True, draw=False)
+    core = TaigaCore(executor, settings, authenticated=True, draw=False)
     assert isinstance(core.controller, controllers.projects.ProjectsController)
     assert core.state_machine.state == StateMachine.PROJECTS
     core.state_machine.project_detail(project)
