d74693e28c1177f4b070098a89955646b53ec37a
David Barragán Merino <david.barragan@kaleidos.net>
2014-11-09 16:56:43 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ from taiga_ncurses import __name__, __description__, __version__
 from setuptools import setup, find_packages
 
 REQUIREMENTS = [
-    "requests==2.2.1",
-    "urwid>=1.2.1",
+    "requests==2.4.3",
+    "urwid>=1.3.0",
     "x256==0.0.3"
 ]
 
